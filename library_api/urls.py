cabdbf012e1dcf0be01eecca515822627648e4ba
skvoter <r00t@skvoter.xyz>
2019-12-04 14:29:44 +0300

    added CSV exports

--- library_api/urls.py
+++ library_api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from library_api.views import GetReader
+from library_api.views import GetReader, GetReadersCSV, GetBooksCSV
+from library_api.models import Book
 
 urlpatterns = [
     path('reader', GetReader.as_view()),
+    path('export_readers_csv', GetReadersCSV.as_view()),
+    path('export_books_csv', GetBooksCSV.as_view()),
 ]
--- library_api/views.py
+++ library_api/views.py
@@ -1,8 +1,13 @@
+from django.db.models import Count
+
 from rest_framework.views import APIView
+from rest_framework.settings import api_settings
+from rest_framework_csv import renderers as r
 from rest_framework.response import Response
 
 from library_api.models import Reader, Book
 from library_api.serializers import ReaderSerializer
+
 
 # Create your views here.
 
@@ -23,3 +28,22 @@ class GetReader(APIView):
         else:
             ser = ReaderSerializer(instance)
             return Response(ser.data)
+
+# Common view for CSV export
+class GetCSV(APIView):
+
+    renderer_classes = (r.CSVRenderer, ) + tuple(api_settings.DEFAULT_RENDERER_CLASSES)
+    queryset = None
+    def get(self, request):
+        return Response(list(self.queryset.all()))
+
+# Readers CSV export
+class GetReadersCSV(GetCSV):
+
+    queryset = Reader.objects.annotate(Count("books")).values()
+
+
+# Books CSV export
+class GetBooksCSV(GetCSV):
+
+    queryset = Book.objects.values()
